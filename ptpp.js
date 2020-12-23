12d364630f560e3842744a2081ac25ef666e6e29
hchauptfeld <haley.hauptfeld@gmail.com>
2020-12-23 16:03:08 -0500

    cleaned up some ui stuff and started virus logic but tbh i have no clue what im doing

--- ptpp.js
+++ ptpp.js
@@ -1,10 +1,7 @@
 
 var playerNames = [];
 
-function getRandomPlayer() {
-    var name = playerNames[Math.floor(Math.random() * playerNames.length)];
-    return name
-}
+var ruleCount = 0;
 
 const ruleDeck = [
     {
@@ -46,16 +43,29 @@ const ruleDeck = [
         "nameRule": true,
         "virusRule": false,
         "RuleString": ", you're the question master. Anytime you ask someone a question and they respond, they have to drink one."
+    },
+    {
+        "nameRule": true,
+        "virusRule": true,
+        "RuleString": ", talk in a British accent until the game tells you to stop"
     }
 
 ];
 
-//This function deletes a list element selected
+
+function getRandomPlayer() {
+    var name = playerNames[Math.floor(Math.random() * playerNames.length)];
+    return name
+}
+
+//This function deletes a player from the game on the click of the 'X' next to the player's name
 function removePlayer(elem) {
     const list = document.getElementById('players-list')
 
-    //This line is needed to get rid of the 'X' in the string after the actual player
-    nameString = elem.innerText.substring(0, elem.innerText.length - 1)
+    //This line is needed to get rid of the 'X' in the string after the actual player name
+    nameString = elem.innerText.substring(0, elem.innerText.length - 2)
+
+    console.log(nameString)
 
 
     const index = playerNames.indexOf(nameString);
@@ -77,28 +87,66 @@ function addPlayerName() {
         playerNames.push(document.getElementById("playerName").value)
 
         const list = document.getElementById('players-list')
-        list.innerHTML = list.innerHTML + "<li id = 'clicks'>" + document.getElementById("playerName").value + "          " +
-            "</input>" + "<button type='button' class = 'btn btn-danger todo-delete' value = clicks onclick = 'removePlayer(this.parentElement)'>X</button></li>"
+        list.innerHTML = list.innerHTML + "<li id = 'clicks'>" + document.getElementById("playerName").value + "          " + "<button type='button' class = 'btn btn-danger todo-delete' value = clicks onclick = 'removePlayer(this.parentElement)'>X</button></li>"
     }
     document.getElementById('playerName').value = ''
+    console.log(playerNames)
+}
 
+//generates a random string that is 5 characters long so that we can ID virus rules to keep track of them
+function generateVirusRuleID() {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < 5; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log(result);
+    return result;
+}
+
+//generates a random int between a min and and max int, (preferably 5 and 10)
+//this function is meant for tracking the amount of rules after a specific virus has been displayed so that it can also end before the game ends
+//probably want to have min always be around 5 and max always be around 10
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getRule() {
     var rule = ruleDeck[Math.floor(Math.random() * ruleDeck.length)];
 
-    if (rule.nameRule == true) {
-        document.getElementById("playername").innerHTML = "<span style='color: #696969; font-size: 50px'>" + getRandomPlayer() + "</span>";
+    if (ruleCount > 10) {
+        document.getElementById("get_rule").style.visibility = "hidden"
+        document.getElementById("rule").innerHTML = "Game Over!"
     } else {
-        document.getElementById('playername').innerHTML = ''
+
+
+        if (rule.virusRule == true) {
+            document.getElementById('virus').innerHTML = "<span style='color: #696969; font-size: 50px'>Virus!</span>";
+        } else {
+            document.getElementById('virus').innerHTML = ''
+        }
+
+
+        if (rule.nameRule == true) {
+            document.getElementById("playername").innerHTML = "<span style='color: #696969; font-size: 50px'>" + getRandomPlayer() + "</span>";
+        } else {
+            document.getElementById('playername').innerHTML = ''
+        }
+
+
+        document.getElementById("rule").innerHTML = "<span style='color: #696969; font-size: 50px'>" + rule.RuleString + "</span>";
+
+        document.body.style.backgroundColor = getGoodLookingColor()
+
+        ruleCount++;
+
+        console.log(ruleCount);
     }
-
-
-    document.getElementById("rule").innerHTML = "<span style='color: #696969; font-size: 50px'>" + rule.RuleString + "</span>";
-
-    document.body.style.backgroundColor = getGoodLookingColor()
 }
 
+
+//This switches the game from the home page with the player names listed to diaplying actual rules
 function startGame() {
     if (playerNames.length == 0) {
         alert("You need to enter at least one name to play!")
@@ -143,6 +191,8 @@ function getColor() {
 */
 
 function getGoodLookingColor() {
-    colorArray = ["#FF474A", "#FFE6A7", "#8C2B76", "#F487B6", "#FDE12D", "#6A8E7F", "#CD8B76", "#76B041", "#F0B67F", "#E1F2FE"]
-    return colorArray[Math.floor(Math.random() * colorArray.length)];
+    colorArray = ["#FF474A", "#FFE6A7", "#F487B6", "#FDE12D", "#6A8E7F", "#CD8B76", "#76B041", "#F0B67F", "#E1F2FE"]
+    var colour = colorArray[Math.floor(Math.random() * colorArray.length)];
+    console.log(colour);
+    return colour;
 }
